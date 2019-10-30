5b2cc23526fe89af4fc456920064f1ff6385f7b6
Stefan Kaufhold <cere@fastmail.fm>
2019-10-30 21:57:29 +0100

    initial commit

---
+++ src/lib.rs
@@ -0,0 +1,198 @@
+//! A bus type (SPMC) channel where multiple consumers can subscribe to single source channel.
+//! The bus can internally use any channel/receiver combination. By default it provides constructor
+//! methods to use the `futures::channel::mpsc` channels.
+
+#![deny(missing_docs, unused_must_use, unused_mut, unused_imports, unused_import_braces)]
+
+use std::pin::Pin;
+use std::sync::{Arc, Weak};
+
+use futures_channel::mpsc;
+use futures_core::task::Context;
+use futures_core::{Poll, Stream};
+use futures_sink::Sink;
+use parking_lot::RwLock;
+use slab::Slab;
+
+/// The struct containing references to the receivers and at the same time the data source for the
+/// bus.
+pub struct FutureBus<T, S, R>
+where
+    T: Send + Clone + 'static,
+    S: Sink<T> + Unpin,
+    R: Stream<Item = T> + Unpin,
+{
+    /// the senders pointing to all the registered subscribers
+    senders: Arc<RwLock<Slab<S>>>,
+    /// factory function to create a new instance of the underlying channel
+    ctor: Box<dyn Fn() -> (S, R) + Send + Sync>,
+}
+
+/// A receiver of data from a [`FutureBus`](self::FutureBus). When it is dropped, the sender is
+/// removed from the bus. Note that this means dropping a receiver causes a write access to the
+/// bus, so it causes a lock.
+pub struct BusSubscriber<T, S, R>
+where
+    T: Send + Clone + 'static,
+    S: Sink<T> + Unpin,
+    R: Stream<Item = T> + Unpin,
+{
+    inner_receiver: R,
+    sender_registry: Weak<RwLock<Slab<S>>>,
+    sender_key: usize,
+}
+
+impl<T, S, R> Stream for BusSubscriber<T, S, R>
+where
+    T: Send + Clone + 'static,
+    S: Sink<T> + Unpin,
+    R: Stream<Item = T> + Unpin,
+{
+    type Item = R::Item;
+
+    fn poll_next(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Option<Self::Item>> {
+        Pin::new(&mut Pin::into_inner(self).inner_receiver).poll_next(cx)
+    }
+}
+
+impl<T, S, R> Drop for BusSubscriber<T, S, R>
+where
+    T: Send + Clone + 'static,
+    S: Sink<T> + Unpin,
+    R: Stream<Item = T> + Unpin,
+{
+    fn drop(&mut self) {
+        if let Some(senders) = self.sender_registry.upgrade() {
+            senders.write().remove(self.sender_key);
+        }
+    }
+}
+
+impl<T, S, R> FutureBus<T, S, R>
+where
+    T: Send + Clone + 'static,
+    S: Sink<T> + Unpin,
+    R: Stream<Item = T> + Unpin,
+{
+    /// Create a new subscriber channel
+    pub fn subscribe(&mut self) -> BusSubscriber<T, S, R> {
+        let (sender, receiver) = self.ctor.as_ref()();
+        let key = self.senders.write().insert(sender);
+        BusSubscriber {
+            inner_receiver: receiver,
+            sender_registry: Arc::downgrade(&self.senders),
+            sender_key: key,
+        }
+    }
+}
+
+/// Create a new bus using bounded channels
+pub fn bounded<T: Send + Clone + 'static>(
+    buffer: usize,
+) -> FutureBus<T, mpsc::Sender<T>, mpsc::Receiver<T>> {
+    FutureBus {
+        senders: Arc::new(RwLock::new(Slab::new())),
+        ctor: Box::new(move || mpsc::channel::<T>(buffer)),
+    }
+}
+
+/// Create a new bus using unbounded channels
+pub fn unbounded<T: Send + Clone + 'static>(
+) -> FutureBus<T, mpsc::UnboundedSender<T>, mpsc::UnboundedReceiver<T>> {
+    FutureBus {
+        senders: Arc::new(RwLock::new(Slab::new())),
+        ctor: Box::new(mpsc::unbounded::<T>),
+    }
+}
+
+impl<T, S, R> Sink<T> for FutureBus<T, S, R>
+where
+    T: Send + Clone + 'static,
+    S: Sink<T> + Unpin,
+    R: Stream<Item = T> + Unpin,
+{
+    type Error = S::Error;
+
+    fn poll_ready(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Pin::into_inner(self)
+            .senders
+            .write()
+            .iter_mut()
+            .map(|(_, sender)| Pin::new(sender).poll_ready(cx))
+            .find_map(|poll| match poll {
+                Poll::Ready(Err(_)) | Poll::Pending => Some(poll),
+                _ => None,
+            })
+            .or_else(|| Some(Poll::Ready(Ok(()))))
+            .unwrap()
+    }
+
+    fn start_send(self: Pin<&mut Self>, item: T) -> Result<(), Self::Error> {
+        Pin::into_inner(self)
+            .senders
+            .write()
+            .iter_mut()
+            .map(|(_, sender)| Pin::new(sender).start_send(item.clone()))
+            .collect::<Result<_, _>>()
+    }
+
+    fn poll_flush(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Pin::into_inner(self)
+            .senders
+            .write()
+            .iter_mut()
+            .map(|(_, sender)| Pin::new(sender).poll_flush(cx))
+            .find_map(|poll| match poll {
+                Poll::Ready(Err(_)) | Poll::Pending => Some(poll),
+                _ => None,
+            })
+            .or_else(|| Some(Poll::Ready(Ok(()))))
+            .unwrap()
+    }
+
+    fn poll_close(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Result<(), Self::Error>> {
+        Pin::into_inner(self)
+            .senders
+            .write()
+            .iter_mut()
+            .map(|(_, sender)| Pin::new(sender).poll_close(cx))
+            .find_map(|poll| match poll {
+                Poll::Ready(Err(_)) | Poll::Pending => Some(poll),
+                _ => None,
+            })
+            .or_else(|| Some(Poll::Ready(Ok(()))))
+            .unwrap()
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use futures::executor::block_on;
+    use futures::{SinkExt, StreamExt};
+
+    use crate::unbounded;
+
+    #[test]
+    fn test_subscribe() {
+        let mut bus = unbounded();
+        let mut r1 = bus.subscribe();
+        let mut r2 = bus.subscribe();
+        block_on(bus.send(10)).unwrap();
+        assert_eq!(block_on(r1.next()), Some(10));
+        assert_eq!(block_on(r2.next()), Some(10));
+    }
+
+    #[test]
+    fn test_drop() {
+        let mut bus = unbounded();
+        let mut r1 = bus.subscribe();
+        {
+            let mut r2 = bus.subscribe();
+            block_on(bus.send(10)).unwrap();
+            assert_eq!(block_on(r1.next()), Some(10));
+            assert_eq!(block_on(r2.next()), Some(10));
+        }
+        block_on(bus.send(15)).unwrap();
+        assert_eq!(block_on(r1.next()), Some(15));
+    }
+}
